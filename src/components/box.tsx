b21845fd1b7ffeecb369b6ef77b1045b3271a842
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2026-04-07 15:36:20 -0400

    chore/box-component (#4)

---
+++ src/components/box.tsx
@@ -0,0 +1,18 @@
+export function Box() {
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100px",
+        height: "100px",
+        borderRadius: "8px",
+      }}
+    >
+      Box
+    </div>
+  );
+}
