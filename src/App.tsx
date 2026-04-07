18fb621d4854fc3192d3f6123a77dd82cc041371
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2026-04-07 15:56:58 -0400

    chore/circle-component (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Box } from "./components/box";
+import { Circle } from "./components/circle";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Welcome to React!</h1>
-      <Box />
+      <h1>Shapes!</h1>
+
+      <div style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
+        <Box bgColor="blue" color="white" />
+        <Circle bgColor="green" color="white" />
+      </div>
     </div>
   );
 }
